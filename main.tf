73f8e50263e70f79ff49d3981e31d26d9cd04787
Nagababu Thota <nagababuthota593@gmail.com>
2023-07-08 17:28:09 +0530

    added provider aliasing

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
     region = "us-east-1"
 }
 
+provider "aws" {
+    region = "us-east-2"
+    alias = "east2"
+}
+
 resource "aws_s3_bucket" "nb-bucket"{
     bucket = "red-bucket"
+    versioning {
+      enabled = true
+    }
+    provider = aws.east2
 }
